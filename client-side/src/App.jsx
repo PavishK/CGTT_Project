ca8d83ab87b81b4978ae7f593bb2b2009ef37c3f
PavishK <kpavish136@gmail.com>
2025-04-07 20:56:34 +0530

    setup Complete

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, repellat quibusdam tenetur assumenda corporis earum nihil unde sequi. Consequuntur aliquid corrupti quibusdam possimus excepturi. Totam esse recusandae quod accusantium veritatis.</div>
+    <div className=''>App</div>
   )
 }
 
