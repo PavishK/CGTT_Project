c37309f508e49990eed1961449135e0c5aa0e18e
PavishK <kpavish136@gmail.com>
2025-06-18 16:47:53 +0530

    Ready for deployment

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
+//import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react(),
-      visualizer({
-      filename: 'visualizer_report.html',    
-      open: true,                         
-      gzipSize: true,                     
-      brotliSize: true,                   
-    }),
+    react()
   ],
 });
 
